14dde89fb4f19ee86d6b9d50fffcb69854bcbe3c
Carlos Serôdio <carlosserodio86@hotmail.com>
2023-05-18 09:56:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# TypeScript VITE MERN
+
+1. Create TypeScript React App By Vite
+   1. npm create vite@latest client -- --template react-ts
+   2. cd client
+   3. npm install
+   4. npm run dev
---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>TS VITE MERN</header>
+      <main></main>
+      <footer>Carlos Serôdio - All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
