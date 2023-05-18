dd50b026bae6ece5b13df4d130b232282cda5ba8
Carlos Serôdio <carlosserodio86@hotmail.com>
2023-05-18 10:33:03 +0100

    List Products

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # TypeScript VITE MERN
 
 1. Create TypeScript React App By Vite
+
    1. npm create vite@latest client -- --template react-ts
    2. cd client
    3. npm install
    4. npm run dev
+
+2. List Products
+
+   1. create Product type
+   2. create products array
+   3. add product images
+   4. render products
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import { products } from "./data";
 
 function App() {
   return (
     <div>
       <header>TS VITE MERN</header>
-      <main></main>
+      <main>
+        <ul>
+          {products.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className='product-image'
+              />
+              <h2>{product.name}</h2>
+              <p>{product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>Carlos Serôdio - All rights reserved</footer>
     </div>
   );
