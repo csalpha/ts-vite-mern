b4ac4f026590b40c6bc1fb69a42416633bc805d9
Carlos Serôdio <carlosserodio86@hotmail.com>
2023-05-18 10:49:55 +0100

    Add React Bootstrap

--- README.md
+++ README.md
@@ -13,3 +13,9 @@
    2. create products array
    3. add product images
    4. render products
+
+3. Add React Bootstrap
+
+   1. npm install react-bootstrap bootstrap
+   2. import bootstrap css
+   3. update App.tsx
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,46 @@
 import "./App.css";
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import { products } from "./data";
 
 function App() {
   return (
-    <div>
-      <header>TS VITE MERN</header>
+    <div className='d-flex flex-column vh-100'>
+      <header>
+        {" "}
+        <Navbar bg='dark' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>TS MERN</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href='/cart' className='nav-link'>
+              Cart
+            </a>
+            <a href='/signin' className='nav-link'>
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {products.map((product) => (
-            <li key={product.slug}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className='product-image'
-              />
-              <h2>{product.name}</h2>
-              <p>{product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className='mt-3'>
+          <Row>
+            {products.map((product) => (
+              <li key={product.slug}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className='product-image'
+                />
+                <h2>{product.name}</h2>
+                <p>{product.price}</p>
+              </li>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>Carlos Serôdio - All rights reserved</footer>
+      <footer>
+        <div className='text-center'>Carlos Serôdio - All rights reserved</div>
+      </footer>
     </div>
   );
 }
